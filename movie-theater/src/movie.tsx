cda23af60703225f8040d7d3e86fd9678dad16da
Guillermo Maschwitz <guilledevel@gmail.com>
2020-12-10 09:30:50 -0300

    final style for list of movies

--- movie-theater/src/movie.tsx
+++ movie-theater/src/movie.tsx
@@ -3,7 +3,10 @@ import {IMovie} from './movie.interface';
 
 function Movie(props: {movie: IMovie}) {
     return (
-        <li className="movie">{props.movie.title}</li>
+        <li 
+            className="movie"
+            style={{backgroundImage: `url(https://image.tmdb.org/t/p/w342/${props.movie.poster_path})`}}
+        ></li>
     );
 }
 
